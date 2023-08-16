cc330e3754fd55f86364574a7ce09be137169ff3
CJ3016994 <CJ3016994@LAB01-PC31>
2023-08-16 10:32:09 -0300

    Adicionar arquivos de projeto.

---
+++ projeto Unhas/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace projeto_Unhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnmessage = new System.Windows.Forms.Button();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnmessage
+            // 
+            this.btnmessage.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnmessage.Font = new System.Drawing.Font("Swis721 Blk BT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnmessage.ForeColor = System.Drawing.Color.MistyRose;
+            this.btnmessage.Location = new System.Drawing.Point(371, 196);
+            this.btnmessage.Name = "btnmessage";
+            this.btnmessage.Size = new System.Drawing.Size(131, 52);
+            this.btnmessage.TabIndex = 0;
+            this.btnmessage.Text = "Entre";
+            this.btnmessage.UseVisualStyleBackColor = false;
+            this.btnmessage.Click += new System.EventHandler(this.btnmessage_Click);
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.BackColor = System.Drawing.Color.MistyRose;
+            this.txbEmail.ForeColor = System.Drawing.Color.Black;
+            this.txbEmail.Location = new System.Drawing.Point(276, 85);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(325, 20);
+            this.txbEmail.TabIndex = 1;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.Location = new System.Drawing.Point(273, 64);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(61, 18);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "E-mail:";
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.BackColor = System.Drawing.Color.MistyRose;
+            this.txbSenha.ForeColor = System.Drawing.Color.Black;
+            this.txbSenha.Location = new System.Drawing.Point(276, 156);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(325, 20);
+            this.txbSenha.TabIndex = 3;
+            this.txbSenha.TextChanged += new System.EventHandler(this.txbsenha_TextChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(273, 135);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(60, 18);
+            this.lblPassword.TabIndex = 4;
+            this.lblPassword.Text = "Senha:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 536);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.btnmessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnmessage;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
+
---
+++ projeto Unhas/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace projeto_Unhas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnmessage_Click(object sender, EventArgs e)
+
+        {
+            string name = txbEmail.Text;
+            string enrollment = txbSenha.Text;
+
+
+            string message = "Nome:" + name +
+                "\nMatrícula:" + enrollment;
+            
+            MessageBox.Show(
+                "Agora você está na nossa plataforma!" ,
+                "Olá seja bem vindo(a)!!",
+                MessageBoxButtons.OK,
+              MessageBoxIcon.Information
+                 );
+        }
+
+        private void txbsenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
